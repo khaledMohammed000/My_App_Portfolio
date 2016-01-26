0e0ebb65dcff430838eb8f42968e75d92956370e
Mohammed Khaled <khaled.mak94@gmail.com>
2016-01-27 02:41:24 +0530

    commit #1  A absic Mockup with the very Less acual functionality . there will be more in the coming commits

---
+++ app/src/main/java/com/practice/tocode/back/myappprofile/MainActivity.java
@@ -0,0 +1,112 @@
+package com.practice.tocode.back.myappprofile;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1 ;
+    Button b2 ;
+    Button b3 ;
+    Button b4 ;
+    Button b5 ;
+    Button b6 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        b1= (Button) findViewById(R.id.button1);
+        b2= (Button) findViewById(R.id.button2);
+        b3= (Button) findViewById(R.id.button3);
+        b4= (Button) findViewById(R.id.button4);
+        b5= (Button) findViewById(R.id.button5);
+        b6= (Button) findViewById(R.id.button6);
+
+        b1.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "opens my spotify Streamer app", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"opens my spotify Streamer app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b3.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"opens my Scores app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b4.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"opens my Build It Bigger app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b5.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"opens my XYZ Reader app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b6.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"opens my CAPSTONE project app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
